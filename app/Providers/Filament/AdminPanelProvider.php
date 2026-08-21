6551da8f31eaa46fcc1010b48553dda1280e0fff
Jay-kod <james1ofourtime@gmail.com>
2026-08-21 15:39:13 +0100

    auto-sync: 2026-08-21 15:39:13 - updated 1 file(s)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\Login;
 use App\Filament\Pages\Auth\Register;
 use App\Filament\Widgets\OnboardingChecklistWidget;
 use App\Models\Account;
