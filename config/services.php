13df9b4344211c3d389092733cab8178a7da2e3b
Jay-kod <james1ofourtime@gmail.com>
2026-08-21 16:31:59 +0100

    auto-sync: 2026-08-21 16:31:58 - updated 1 file(s)

--- config/services.php
+++ config/services.php
@@ -35,4 +35,16 @@ return [
         ],
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT_URI', env('APP_URL', 'http://127.0.0.1:8000') . '/auth/callback/github'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI', env('APP_URL', 'http://127.0.0.1:8000') . '/auth/callback/google'),
+    ],
+
 ];
