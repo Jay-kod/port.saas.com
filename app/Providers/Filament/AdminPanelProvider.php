c65870e4e013ee239fd125dacb2b397dc5ff1020
Jay-kod <james1ofourtime@gmail.com>
2026-08-21 15:38:04 +0100

    auto-sync: 2026-08-21 15:38:04 - updated 1 file(s)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -51,6 +51,8 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandName('DevFolio.AI')
+            ->brandLogo(fn () => view('filament.components.brand-logo'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
