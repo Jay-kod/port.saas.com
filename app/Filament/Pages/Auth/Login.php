8cf467a2aa5c824739c5935f5c298b4943f3e7a6
Jay-kod <james1ofourtime@gmail.com>
2026-08-21 15:53:24 +0100

    auto-sync: 2026-08-21 15:53:24 - updated 1 file(s)

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -10,6 +10,16 @@ class Login extends BaseLogin
 {
     protected string $view = 'filament.pages.auth.login';
 
+    public function getMaxWidth(): ?string
+    {
+        return '6xl';
+    }
+
+    public function hasLogo(): bool
+    {
+        return false;
+    }
+
     public function getHeading(): string|Htmlable
     {
         return 'DevFolio Portal Sign In';
