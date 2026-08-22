70c644146f75976d717ecf27464b9372a867657e
Jay-kod <james1ofourtime@gmail.com>
2026-08-22 17:16:28 +0100

    auto-sync: 2026-08-22 17:16:28 - updated 1 file(s)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'resolve.tenant' => \App\Http\Middleware\ResolveTenantFromSlug::class,
             'super_admin' => \App\Http\Middleware\EnsureSuperAdmin::class,
+            'prevent.back' => \App\Http\Middleware\PreventBackHistory::class,
         ]);
         $middleware->redirectTo(guests: '/admin/login');
     })
